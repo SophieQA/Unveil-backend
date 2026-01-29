2a187ec4a1e84f16dae10270f6418a732eba82d7
Qiusi Agre <qiusi.agre@icloud.com>
2026-01-29 16:19:52 -0500

    initial deployment

--- src/main/java/com/unveil/UnveilApplication.java
+++ src/main/java/com/unveil/UnveilApplication.java
@@ -2,10 +2,18 @@ package com.unveil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UnveilApplication {
     public static void main(String[] args) {
         SpringApplication.run(UnveilApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
     }
 }
