bef870b32e8be0a294a6e62150544cb30621771f
Qiusi Agre <qiusi.agre@icloud.com>
2026-02-05 00:44:00 -0500

    add ArtworkDto class for artwork data transfer

---
+++ src/main/java/com/unveil/dto/ArtworkDto.java
@@ -0,0 +1,23 @@
+package com.unveil.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArtworkDto {
+
+    private String artworkId;
+    private String title;
+    private String artist;
+    private String imageUrl;
+    private String museumSource;
+    private String objectDate;
+    private String medium;
+    private String dimensions;
+    private String creditLine;
+}
